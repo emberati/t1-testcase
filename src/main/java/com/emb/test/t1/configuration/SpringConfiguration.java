c9c7808c0db826900a8c01bd925a0565b8724c42
emberati <alex.shak55@gmail.com>
2023-09-12 13:31:36 +0300

    Implemented configuration bean for `ModelMapper`

---
+++ src/main/java/com/emb/test/t1/configuration/SpringConfiguration.java
@@ -0,0 +1,16 @@
+package com.emb.test.t1.configuration;
+
+import com.emb.test.t1.mapping.FrequencySetToObjectNodeConverter;
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringConfiguration {
+    @Bean
+    public ModelMapper modelMapper() {
+        var mapper = new ModelMapper();
+        mapper.addConverter(new FrequencySetToObjectNodeConverter());
+        return mapper;
+    }
+}
